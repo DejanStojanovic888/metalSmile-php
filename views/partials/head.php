a2c13eff2d9da8d2b05e8e495ceb3b4a40c5c0d8
Dejan <zasluzujene@gmail.com>
2026-01-06 21:43:12 +0100

    small change on main.css path

--- views/partials/head.php
+++ views/partials/head.php
@@ -8,7 +8,7 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <!--    ctrl + \  pa se otvori novi input prozor u koji upisemo: b5 cdn   Samo ovo meni nije radilo. Ubacuje bootstrap5. -->
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="/css/main.css">
 
 </head>
 <body>
